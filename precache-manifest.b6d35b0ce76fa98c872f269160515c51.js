3e1b6b286346c7864841402a0c879a57ed4c9a5d
mahme <mustafasaleh.hf@gmail.com>
2020-06-06 16:58:48 +0200

    Updates

---
+++ precache-manifest.b6d35b0ce76fa98c872f269160515c51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e85a944acd4a22096df1233ec1e86795",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "bfb56a4237aa5d3cf814",
+    "url": "/hello-react/static/css/main.3e708055.chunk.css"
+  },
+  {
+    "revision": "d3bf2e225c8f0bc624f6",
+    "url": "/hello-react/static/js/2.4766db7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-react/static/js/2.4766db7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb56a4237aa5d3cf814",
+    "url": "/hello-react/static/js/main.85779673.chunk.js"
+  },
+  {
+    "revision": "d2e3280283877fd484a7",
+    "url": "/hello-react/static/js/runtime-main.80cc958b.js"
+  }
+]);
